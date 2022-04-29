a7282a38786834b7bd622ddac4e1485ce123fc7d
ancs214 <ancs214@gmail.com>
2022-04-29 17:19:32 -0400

    created POST routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
 
-//here we are setting the prefix '/users' so that we dont have to in all of our routes in user-routes.js
+//here we are setting the prefix '/users' and '/posts' so that we dont have to in all of our routes in user-routes.js/post-routes.js
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+
 
 module.exports = router;
