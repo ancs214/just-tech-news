106c5e496cd30c581ac5b920216375327002776a
ancs214 <ancs214@gmail.com>
2022-05-02 12:41:02 -0400

    begin setup for handlbars

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+//for path.join
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//express.static is middleware function that can take all of the contents of a folder and serve them as static assets.
+app.use(express.static(path.join(__dirname, 'public')));
+
+//for handlebars template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
